c661d196dd2620c7e79643f2daf443c5dd7c124c
zhaojiu <1821096329@qq.com>
2021-07-05 23:05:32 +0800

    第三次提交，添加医院CRUD相关接口

---
+++ common/service-util/src/main/java/com/zj/yygh/common/handle/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.zj.yygh.common.handle;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author 赵赳
+ * @CreateTime: 2021/3/8 23:37
+ * @Description:
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        //设置自动填充属性
+        this.setFieldValByName("createTime",new Date(),metaObject);
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+        this.setFieldValByName("isDeleted",0,metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+}
